7e1b9839f4e8e9e0cb783acc7aeac22eaea0f43d
Tyythedeveloper33 <webstertyler81@yahoo.com>
2024-09-05 18:32:18 -0400

    set up routes properly

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
+const routes = require('./routes');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -18,3 +18,27 @@ if (!isProduction) {
     // enable cors only in development
     app.use(cors());
   }
+
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+  // backend/app.js
+
+
+// ...
+
+app.use(routes);
+module.exports = app;
